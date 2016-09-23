256241bebd5974f7a457b83c49025edc24ab0104
Joonas Loppi <joonas@joonas.fi>
2016-09-23 17:09:45 +0300

    preview.sh: do not try to hog common ports like 80 or 8080

--- preview.sh
+++ preview.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -eu
 
-echo "Hit this URL in your momma's browser: joonas-blog.dev-laptop.xs.fi"
+echo "Hit this URL in your momma's browser: http://joonas-blog.dev-laptop.xs.fi:8089"
 
-docker run --rm -it -v "$(pwd):/project" -e VIRTUAL_HOST=joonas-blog.dev-laptop.xs.fi -p 8080 joonas/jekyll-builder:0.1.0 jekyll serve -H 0.0.0.0 -P 8080 --source /project/blog/ --destination /tmp
+docker run --rm -it -v "$(pwd):/project" -e VIRTUAL_HOST=joonas-blog.dev-laptop.xs.fi -p 8089:8089 joonas/jekyll-builder:0.1.0 jekyll serve -H 0.0.0.0 -P 8089 --source /project/blog/ --destination /tmp
