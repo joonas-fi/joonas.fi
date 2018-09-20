4d16674dce82514ac11e9d8162474dba8c5004e0
Joonas Loppi <joonas@joonas.fi>
2018-09-20 14:23:34 +0300

    Try to fix build

--- bin/build.sh
+++ bin/build.sh
@@ -3,3 +3,7 @@
 rm -rf build/
 mkdir build
 jekyll build --source blog/ --destination build/
+
+# Docker build fails without this with error msg:
+# no permission to read from '/tmp/bob/joonas.fi-blog/workspace/.sass-cache/...
+rm -rf .sass-cache
