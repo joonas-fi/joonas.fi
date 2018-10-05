ee34c024c04ebe986acb364fd0794a030190001b
Joonas Loppi <joonas@joonas.fi>
2018-10-05 10:48:32 +0300

    Update preview mode

---
+++ bin/preview.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -eu
+
+jekyll serve -H 0.0.0.0 -P 80 --source /project/blog/ --destination /tmp
--- preview.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -eu
-
-echo "Hit this URL in your momma's browser: http://joonas-blog.dev-laptop.xs.fi:8089"
-
-docker run --rm -it -v "$(pwd):/project" -p 8089:8089 joonas/jekyll-builder:0.1.0 jekyll serve -H 0.0.0.0 -P 8089 --source /project/blog/ --destination /tmp
