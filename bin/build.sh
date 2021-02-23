be9a3a61e48b9690bb19e796f2c9235004ddf9fd
Joonas Loppi <joonas@joonas.fi>
2021-02-23 10:21:44 +0200

    fix RSS feed

--- bin/build.sh
+++ bin/build.sh
@@ -8,4 +8,11 @@ mkdir -p rel/
 
 hugo
 
+# the root-level RSS feed includes about/contact pages etc, and there's no easy way to fix it without
+# overriding (and taking ownership of) the RSS template, so replace the root-level (actually, we also
+# disabled rednering it) feed with the blog-specific feed instead.
+#
+# https://www.refactoredtelegram.net/2020/12/posts-only-rss-feed-in-hugo/
+mv public/posts/feed.xml public/feed.xml
+
 (cd public/ && tar -czf ../rel/site.tar.gz *)
